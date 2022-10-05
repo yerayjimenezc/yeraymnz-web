6770adb14f32bccd6d22a611be81d66b9cbaeb3c
yerayjimenezc <yerayjimenezcarmona@gmail.com>
2022-10-05 18:31:00 +0200

    Header responsive completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-layout>
-      <v-app-bar color="primary"></v-app-bar>
-      <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+      <v-app-bar>
+        <v-col v-if="$vuetify.display.mobile" cols="1">
+          <v-btn @click.stop="drawer = !drawer" icon="mdi-dots-vertical"></v-btn>
+        </v-col>
+        <v-spacer v-if="$vuetify.display.mobile" />
+        <v-app-bar-title class="d-flex ma-0 justify-center"><img src="./assets/logoMNZ.svg" width="50">
+        </v-app-bar-title>
+        <v-spacer />
+        <v-col cols="1" v-if="$vuetify.display.mobile">
+        </v-col>
+        <v-col v-if="!$vuetify.display.mobile">
+          <v-list class="d-flex" density="compact" nav>
+            <v-list-item prepend-icon="mdi-alpha-i" title="Sobre mí" value="#"></v-list-item>
+            <v-list-item prepend-icon="mdi-alpha-v" title="Proyectos" value="#"></v-list-item>
+            <v-list-item prepend-icon="mdi-alpha-x" title="Contacto" value="#"></v-list-item>
+          </v-list>
+        </v-col>
+      </v-app-bar>
+      <v-navigation-drawer color="grey-darken-2" v-model="drawer" temporary>
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-alpha-i" title="Sobre mí" value="#"></v-list-item>
+          <v-list-item prepend-icon="mdi-alpha-v" title="Proyectos" value="#"></v-list-item>
+          <v-list-item prepend-icon="mdi-alpha-x" title="Contacto" value="#"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
       <v-main>
         <HelloWorld />
       </v-main>
@@ -12,16 +35,27 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-
   components: {
     HelloWorld,
   },
-
   data: () => ({
-    //
-  }),
+    drawer: false
+  })
 }
 </script>
+
+<style>
+body {
+  font-family: 'Bitter', serif;
+}
+
+#navbarlist {
+  list-style: none;
+}
+
+li {
+  text-decoration: none;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,37 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <h1>Soy el componente hijo, {{msg}}</h1>
+    <v-row>
+      <v-col class="d-flex flex-column justify-center" v-if="!$vuetify.display.mobile" cols="12" sm="6">
+        <p>Hola, soy</p>
+        <h1>Yeray Jiménez</h1>
+        <h3>🚀Construyo cosas para web.</h3>
+        <v-btn class="mt-4 w-50" rounded="lg" variant="outlined" size="x-large" color="primary">
+          Ver Proyectos!
+        </v-btn>
+      </v-col>
+      <v-col class="d-flex flex-column align-center" cols="12" sm="6">
+        <img id="avatar" width="400" src="../assets/Avatar portfolio web.png" alt="Avatar">
+      </v-col>
+      <v-col class="d-flex flex-column align-center justify-center" v-if="$vuetify.display.mobile" cols="12" sm="6">
+        <p>Hola, soy</p>
+        <h1>Yeray Jiménez</h1>
+        <h3>🚀Construyo cosas para web.</h3>
+        <v-btn class="mt-4" rounded="lg" variant="outlined" size="x-large" color="primary">
+          Ver Proyectos!
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="my-5 mx-2">
+      <h4 class="d-flex">
+        <p class="text-primary">I.</p>
+        <p class="font-weight-light">Sobre mí</p>
+      </h4>
+      <v-col class="d-flex flex-column justify-center" cols="12" sm="6">
+
+      </v-col>
+      <v-col class="d-flex flex-column align-center" cols="12" sm="6">
+
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -16,3 +46,15 @@ export default {
   }),
 }
 </script>
+
+<style>
+#avatar {
+  filter: grayscale();
+  transition: 0.3s;
+}
+
+#avatar:hover {
+  filter: none;
+  transition: 0.3s;
+}
+</style>
