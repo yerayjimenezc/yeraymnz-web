c612adb093863d786a01b855993ca98094facf7a
yerayjimenezc <yerayjimenezcarmona@gmail.com>
2022-10-05 14:40:49 +0200

    Basic project, vue3&vite&vuetify themedark

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-layout>
+      <v-app-bar color="primary"></v-app-bar>
+      <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+      <v-main>
+        <HelloWorld />
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <h1>Soy el componente hijo, {{msg}}</h1>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    msg: 'yeray'
+  }),
+}
+</script>
